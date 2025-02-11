dd38e9bb64a095bf38b91ad9f14aef02eacf21de
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-02-11 17:50:44 +0200

    Update

--- src/scripts/components/slider.ts
+++ src/scripts/components/slider.ts
@@ -1,14 +1,33 @@
-//@ts-expect-error ignore swiper types
-import Swiper, { Navigation, Autoplay, A11y } from "swiper";
-
 const testimonialsElement = document.querySelector(".testimonials__slider");
 if (testimonialsElement) {
+  //@ts-expect-error ignore swiper types
   new Swiper(testimonialsElement, {
-    modules: [Navigation, Autoplay, A11y],
+    navigation: {
+      prevEl: ".testimonials__nav-button_prev",
+      nextEl: ".testimonials__nav-button_next",
+    },
     slidesPerView: 3,
     spaceBetween: 80,
     speed: 800,
     loop: true,
     centeredSlides: true,
+    breakpoints: {
+      320: {
+        spaceBetween: 45,
+        slidesPerView: 1.2,
+      },
+      640: {
+        spaceBetween: 65,
+        slidesPerView: 1.7,
+      },
+      992: {
+        spaceBetween: 80,
+        slidesPerView: 2.1,
+      },
+      1250: {
+        spaceBetween: 80,
+        slidesPerView: 3,
+      },
+    },
   });
 }
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,18 @@
 import "../styles/main.scss";
-import Tab from "./components/tabs";
 
+import Tab from "./components/tabs";
 import "./components/slider";
+import Lenis from "lenis";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const tabs = new Tab();
-  tabs?.init();
+  if (typeof Tab === "function") {
+    const tabs = new Tab();
+    tabs?.init();
+  }
+
+  if (typeof Lenis === "function") {
+    new Lenis({
+      autoRaf: true,
+    });
+  }
 });
